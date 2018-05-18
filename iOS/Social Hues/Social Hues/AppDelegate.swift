4acf40d3b6fd6859c3920d768b6c8b86564f89da
Daniel Kim <dankim025@gmail.com>
2018-05-17 23:53:23 -0700

    onboarding implemented

--- iOS/Social Hues/Social Hues/AppDelegate.swift
+++ iOS/Social Hues/Social Hues/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  Social Hues
 //
 //  Created by Sarah Zhou on 5/14/18.
-//  Copyright © 2018 Sarah Zhou. All rights reserved.
+//  Copyright © 2018 Social Hues. All rights reserved.
 //
 
 import UIKit
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = OnboardPageViewController()
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ iOS/Social Hues/Social Hues/OnboardPageViewController.swift
@@ -0,0 +1,85 @@
+//
+//  OnboardPageViewController.swift
+//  Social Hues
+//
+//  Created by Daniel Kim on 5/17/18.
+//  Copyright © 2018 Social Hues. All rights reserved.
+//
+
+import UIKit
+
+// OnboardPageView Controller controls the onboarding pages found in the Onboarding storyboard
+class OnboardPageViewController : UIPageViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+        
+        if let firstViewController = controllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+    
+    init() {
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    }
+    
+    // The three view controllers referred by their names
+    private(set) lazy var controllers: [UIViewController] = {
+        return [self.newColoredViewController(order: "One"),
+                self.newColoredViewController(order: "Two"),
+                self.newColoredViewController(order: "Three")]
+    }()
+    
+    private func newColoredViewController(order: String) -> UIViewController {
+        return UIStoryboard(name: "Onboarding", bundle: nil).instantiateViewController(withIdentifier:"Onboard\(order)")
+    }
+}
+
+extension OnboardPageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let vcIndex = controllers.index(of:viewController) else {
+            return nil
+        }
+        let prevIndex = vcIndex - 1
+        
+        guard prevIndex >= 0 else {
+            return nil
+        }
+        
+        guard controllers.count > prevIndex else {
+            return nil
+        }
+        
+        return controllers[prevIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let vcIndex = controllers.index(of:viewController) else {
+            return nil
+        }
+        let nextIndex = vcIndex + 1
+        
+        guard nextIndex != controllers.count else {
+            return nil
+        }
+        
+        guard controllers.count > nextIndex else {
+            return nil
+        }
+        
+        return controllers[nextIndex]
+    }
+    
+    
+    
+}
