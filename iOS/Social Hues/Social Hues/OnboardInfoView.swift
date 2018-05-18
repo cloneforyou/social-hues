95769ba75c44ee671028bcd2a7ef986a031c4161
Daniel Kim <dankim025@gmail.com>
2018-05-17 20:27:24 -0700

    stuff

---
+++ iOS/Social Hues/Social Hues/OnboardInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardInfoView.swift
+//  Social Hues
+//
+//  Created by Daniel Kim on 5/16/18.
+//  Copyright © 2018 Sarah Zhou. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OnboardInfoView: UIView {
+    let nibName = "OnboardInfoView"
+    var contentView: UIView?
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
